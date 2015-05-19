f3937986ecb68b9b95be0e9880a718e978e5c572
JP Richardson <jprichardson@gmail.com>
2015-05-19 15:39:45 -0500

    initial implementation

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 React.js Password
 =================
 
-This is simply a component to show/hide a password field.
+A React.js component to for password fields. At this moment it only allows you to show/hide
+a password field. In the future, it may include more like validation.
 
 
 Usage
 -----
 
     npm i --save react-password
+
+See Examples here: <examples folder>
+
+
+### Example
+
+```html
+<ReactPassword revealed={ this.state.checked } maxLength="25" onChange={ this.handlePasswordChange } id="secret-password"/>
+```
 
 
 License
--- examples/basic/index.js
+++ examples/basic/index.js
@@ -4,15 +4,30 @@ var ReactPassword = require('../../')
 var App = React.createClass({
   displayName: 'App',
 
+  getInitialState: function () {
+    return { checked: false }
+  },
+
+  handleCheckChange: function (event) {
+    this.setState({checked: !this.state.checked})
+  },
+
+  handlePasswordChange: function (event) {
+    console.log(this.refs.password.state.value)
+  },
+
   render: function () {
     return (
       <div>
-        <ReactPassword />
+        <ReactPassword ref="password" value='super secret' revealed={ this.state.checked } maxLength="25" onChange={ this.handlePasswordChange } id="secret-password"/>
+        <br/>
+        <input type='checkbox' onChange= { this.handleCheckChange } checked={ this.state.checked ? 'checked' : null } />
+        Reveal password.
+        <br/>
       </div>
     )
   }
 })
-
 
 window.onload = function () {
   React.render(
@@ -20,5 +35,3 @@ window.onload = function () {
     document.querySelector('.app')
   )
 }
-
-console.log('hi')
